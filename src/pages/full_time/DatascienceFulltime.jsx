262c2bec4ead3f4f6428c0446b24d16122925878
Ramakanth <116887978+ramakanth009@users.noreply.github.com>
2025-05-07 12:29:01 +0530

    certificate

--- src/pages/full_time/DatascienceFulltime.jsx
+++ src/pages/full_time/DatascienceFulltime.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../components/common/navbar/Navbar'
 import Footer from '../../components/common/footer/Footer'
 import TestimonialComponent from "../../components/common/testimonialcomponent/TestimonialComponent";
 import DataScienceIndustryFacts from "../../components/fulltime/datascience/industryfacts/DataScienceIndustryFacts";
-import DataScienceProgramFee from "../../components/fulltime/datascience/programfee/DataScienceProgramFee";
+import DataScienceProgramFee from "../../components/common/fulltime/programfee/ProgramFee";
 import FulltimeCertificates from '../../components/common/fulltime/fulltimeCertificates/FulltimeCertificates';
 
 
--- src/pages/full_time/FullstackFulltime.jsx
+++ src/pages/full_time/FullstackFulltime.jsx
@@ -3,8 +3,8 @@ import FullStackHero from '../../components/fulltime/fullstack/fullstackhero/Ful
 import Navbar from '../../components/common/navbar/Navbar'
 import Footer from '../../components/common/footer/Footer'
 import TestimonialComponent from '../../components/common/testimonialcomponent/TestimonialComponent'
-import FullStackProgramFee from '../../components/fulltime/fullstack/programfee/FullStackProgramFee'
-import FullStackIndustryFacts from '../../components/fulltime/fullstack/industryfacts/FullStackIndustryFacts'
+import FullStackProgramFee from '../../components/common/fulltime/programfee/ProgramFee'
+import FullStackIndustryFacts from '../../components/common/fulltime/industryfacts/IndustryBackedFacts'
 import FulltimeCertificates from '../../components/common/fulltime/fulltimeCertificates/FulltimeCertificates';
 
 
