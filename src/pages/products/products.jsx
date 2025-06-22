59503484e19b1f97f82c06299373d9c6179c2131
Your Name <you@example.com>
2025-06-22 22:14:15 +0530

    latest

---
+++ src/pages/products/products.jsx
@@ -0,0 +1,26 @@
+// src/pages/homepage/Homepage.jsx
+import React from "react";
+import { Box } from "@mui/material";
+import HeroSection from "../../pages/resumelanding/HeroSection";
+import WhyUseSection from "../../pages/resumelanding/WhyUseSection";
+import KeyHighlightsSection from "../../pages/resumelanding/KeyHighlightsSection";
+import ComparisonTableSection from "../../pages/resumelanding/ComparisonTableSection";
+import TemplatesGallerySection from "../../pages/resumelanding/TemplatesGallerySection";
+import WhoIsItFor from "../../pages/resumelanding/WhoIsItFor";
+import SmartResumeSection from "../../pages/resumelanding/SmartResumeSection";
+
+const Procucts = () => {
+  return (
+    <Box>
+      <HeroSection />
+      <WhyUseSection />
+      <KeyHighlightsSection />
+      <TemplatesGallerySection />
+      <WhoIsItFor />
+      <SmartResumeSection />
+      <ComparisonTableSection />
+    </Box>
+  );
+};
+
+export default Procucts;
